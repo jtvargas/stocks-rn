381ee9f6b32ac8c41ac54c9c83aac75e7231c395
Jonathan Taveras <jonathanrtv@outlook.com>
2024-07-05 16:23:55 -0400

    feat: eject expo app and use development build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,6 +13,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
